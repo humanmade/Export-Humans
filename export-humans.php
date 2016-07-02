a8755a2bc4de42741b908745839d2d861acd1d64
Paul Gibbs <djpaul@gmail.com>
2016-07-02 13:16:40 +0200

    Finish up report generation and data formatting.

--- export-humans.php
+++ export-humans.php
@@ -23,6 +23,11 @@ function admin_init() {
 	}
 
 	add_action( bp_core_admin_hook(), __NAMESPACE__ . '\\add_admin_menu' );
+
+	add_filter( 'export_humans_profile_field_value', 'convert_chars'      );
+	add_filter( 'export_humans_profile_field_value', 'force_balance_tags' );
+	add_filter( 'export_humans_profile_field_value', 'xprofile_filter_format_field_value',         1, 2 );
+	add_filter( 'export_humans_profile_field_value', 'xprofile_filter_format_field_value_by_type', 8, 3 );
 }
 add_action( 'bp_init', __NAMESPACE__ . '\\admin_init' );
 
@@ -32,25 +37,26 @@ add_action( 'bp_init', __NAMESPACE__ . '\\admin_init' );
  * @since 1.0
  */
 function add_admin_menu() {
-	add_users_page(
+	$hook = add_users_page(
 		_x( 'Member Profile Data Reports', 'wp-admin screen title', 'export-humans' ),
 		_x( 'Profile Reports', 'wp-admin menu label', 'export-humans' ),
 		'manage_options',
 		'export-humans',
-		__NAMESPACE__ . '\\reports_screen_handler'
+		__NAMESPACE__ . '\\reports_screen'
 	);
+
+	add_action( "load-$hook", __NAMESPACE__ . '\\maybe_generate_report' );
 }
 
 /**
- * Handle form submission/report generation/or display of the reports screen.
+ * Handle form submission/report generation.
  */
-function reports_screen_handler() {
+function maybe_generate_report() {
 	if ( ! bp_current_user_can( 'bp_moderate' ) ) {
 		return;
 	}
 
 	if ( empty( $_GET['action'] ) || $_GET['action'] !== 'report' || empty( $_POST['ids'] ) ) {
-		reports_screen();
 		return;
 	}
 
@@ -152,26 +158,50 @@ function reports_screen() {
  * @param array $profile_field_ids Array of integers.
  */
 function generate_report( $profile_field_ids ) {
-	$data     = array();
-	$headings = array();
-	$users    = get_users( array( 'fields' => array( 'display_name', 'ID' ) ) );
+	$filename   = sanitize_file_name( sprintf( 'facts-report-%s', date( 'Y-m-d-U' ) ) );
+	$first_loop = true;
+	$headings   = array( __( 'Name', 'export-humans' ) );
+	$report     = array();
+	$users      = get_users( array( 'fields' => array( 'display_name', 'ID' ) ) );
 
+
+	// Generate the report.
 	foreach ( $users as $user ) {
 		if ( ! bp_is_user_active( $user->ID ) ) {
 			continue;
 		}
 
-		$data[ "{$user->display_name}" ] = array();
+		$report[ "{$user->display_name}" ] = array( $user->display_name );
 
 		foreach ( $profile_field_ids as $field_id ) {
 			$field      = xprofile_get_field( $field_id );
-			$field_data = trim( xprofile_get_field_data( $field_id, $user->ID ) );
+			$field_data = xprofile_get_field_data( $field_id, $user->ID );
+			$field_data = apply_filters( 'export_humans_profile_field_value', $field_data, $field->type, $field->id );
 
-			$headings[] = $field->name;
-			$data[ "{$user->display_name}" ][] = $field_data;
+			$report[ "{$user->display_name}" ][] = $field_data;
+
+			if ( $first_loop ) {
+				$headings[] = $field->name;
+			}
 		}
+
+		$first_loop = false;
 	}
 
 
+	// Send the report.
+	header( 'Content-Description: File Transfer' );
+	header( "Content-Disposition: attachment; filename={$filename}.csv" );
+	header( 'Content-Type: text/csv; charset=' . get_option( 'blog_charset' ), true );
+
+	$fh = @fopen( 'php://output', 'w' );
+	fwrite( $fh, "\xEF\xBB\xBF" );
+	fputcsv( $fh, $headings );
+
+	foreach ( $report as $data ) {
+		fputcsv( $fh, $data );
+	}
+
+	fclose( $fh );
 	exit;
 }
